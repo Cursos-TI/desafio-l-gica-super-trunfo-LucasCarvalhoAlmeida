f7c4024f7d71ceb58cd94d135139b2a5c188451c
LucasCarvalhoAlmeida <lucas.almeida_bol@hotmail.com>
2025-02-18 00:25:00 +0000

    inicio do programa

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,14 +1,62 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    char estadoa, estadob;
+    char nomecidadea, nomecidadeb;
+    int populacaoa[100], populacaob[100];
+    float areaa, areab;
+    float piba, pibb;
+    int pturisticosa, pturisticosb;
 
+
+    printf("Bem-vindo ao sistema de cadastro do Super Trunfo - Países!\n");
+    printf("\nJogador 1 insira os dados da sua carta\n");
+
+    printf("\nInsira o nome do estado\n");
+    scanf("%c", &estadoa);
+
+    printf("\nDigite o nome da cidade:\n");
+    scanf("%c", &nomecidadea);
+
+    printf("\nDigite a população da cidade:\n");
+    scanf("%d", &populacaoa);
+
+    printf("\nDigite a área da cidade (em km²):\n");
+    scanf("%f", &areaa);
+
+    printf("\nDigite o PIB da cidade (em milhões):\n");
+    scanf("%f", &piba);
+
+    printf("\nDigite o número de pontos turisticos:\n");
+    scanf("%d", &pturisticosa);
+
+
+    printf("\nMuito bom\n");
+    printf("\nAgora é a sua vez jogador 2\n");
+
+    printf("\nInsira o nome do estado\n");
+    scanf("%c", &estadob);
+
+    printf("\nDigite o nome da cidade:\n");
+    scanf("%c", &nomecidadeb);
+
+    printf("\nDigite a população da cidade:\n");
+    scanf("%d", &populacaob);
+
+    printf("\nDigite a área da cidade (em km²):\n");
+    scanf("%f", &areab);
+
+    printf("\nDigite o PIB da cidade (em milhões):\n");
+    scanf("%f", &pibb);
+
+    printf("\nDigite o número de pontos turisticos:\n");
+    scanf("%d", &pturisticosb);
+
+    
+   
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
