eb41ca66649fba9710a9c8dc53d18f6db1e2c313
LucasCarvalhoAlmeida <lucas.almeida_bol@hotmail.com>
2025-02-25 23:57:20 +0000

    interativo

--- Menus_Interativos/interativo.c
+++ Menus_Interativos/interativo.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
 
     int opcao;
+    int numeroSecreto, palpite;
 
     printf("Menu Principal\n");
     printf("1. Iniciar Jogo\n");
     printf("2. Ver Regras\n");
     printf("3. Sair\n");
-    printf("Escolha uam Opção\n");
+    printf("Escolha uma Opção\n");
     scanf("%d", &opcao); 
 
-    
-
-
+    switch (opcao)
+    {
+    case 1:
+        srand(time(0));
+        numeroSecreto = rand() % 10;
+        printf("Digite um número de 0 a 9: ");
+        scanf("%d", &palpite);
+        if (numeroSecreto == palpite)
+        {
+            printf("Você Acertou!\n");
+            printf("Número secreto: %d\n", numeroSecreto);
+        } else{
+            printf("Você Errou!\n");
+            printf("Número secreto: %d\n", numeroSecreto);
+        }
+        break;
+    case 2:
+        printf("As regras são..\n");
+        break;
+    case 3:
+        printf("Sair do jogo.\n");
+        break;
+    default:
+        printf("Opção Inválida.\n");
+        break;
+    }
 
     return 0;
 
