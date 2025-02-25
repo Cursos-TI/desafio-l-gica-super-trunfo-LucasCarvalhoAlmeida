154019c3391a3a093417844426e34ccb62ac6bc0
LucasCarvalhoAlmeida <lucas.almeida_bol@hotmail.com>
2025-02-25 20:05:00 +0000

    Menu_interativo

---
+++ Menus_Interativos/interativo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int opcao;
+
+    printf("Menu Principal\n");
+    printf("1. Iniciar Jogo\n");
+    printf("2. Ver Regras\n");
+    printf("3. Sair\n");
+    printf("Escolha uam Opção\n");
+    scanf("%d", &opcao); 
+
+    
+
+
+
+    return 0;
+
+}
